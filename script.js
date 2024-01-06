02c4e29394921e3ed96f94ddfb0a5e9a89a03ae2
Natalie <natalie.dflcn@gmail.com>
2024-01-05 21:08:17 -0500

    Fixed code

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape') {
-    if (modal.classList.contains('hidden')) {
+    if (!modal.classList.contains('hidden')) {
       closeModal();
     }
   }
