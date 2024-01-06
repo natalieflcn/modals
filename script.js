50efda88a6d0da17c119c4dce45215338e5dfc59
Natalie <natalie.dflcn@gmail.com>
2024-01-05 20:29:07 -0500

    Added event listeners to button

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('button clicked');
+  });
+}
